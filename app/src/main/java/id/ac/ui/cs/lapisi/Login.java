ef671d0b9c785dc3de6312056fb0db2f1f2d4f72
idiotme <meityadianti@gmail.com>
2017-07-12 11:53:41 +0700

    bug fixing login page

--- app/src/main/java/id/ac/ui/cs/lapisi/Login.java
+++ app/src/main/java/id/ac/ui/cs/lapisi/Login.java
@@ -9,10 +9,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
 
@@ -21,27 +24,52 @@ import org.json.JSONObject;
 public class Login extends AppCompatActivity {
 
     private Context context;
-    private TextView tvUsername;
-    private TextView tvPassword;
+    private EditText etUsername;
+    private EditText etPassword;
     private SharedPreferences pref;
+    Validation val = new Validation();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(id.ac.ui.cs.lapisi.R.layout.login);
         context = getApplicationContext();
-        tvUsername = (TextView) findViewById(id.ac.ui.cs.lapisi.R.id.username);
-        tvPassword = (TextView) findViewById(id.ac.ui.cs.lapisi.R.id.password);
-        tvUsername.setSingleLine(true);
+        etUsername = (EditText) findViewById(id.ac.ui.cs.lapisi.R.id.username);
+        // TextWatcher would let us check validation error on the fly
+
+        etUsername.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                val.hasText(etUsername);
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
+
+//        etUsername.setSingleLine(true);
+
+        etPassword = (EditText) findViewById(id.ac.ui.cs.lapisi.R.id.password);
+        etPassword.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                val.hasText(etPassword);
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
+
         final Button signin = (Button) findViewById(id.ac.ui.cs.lapisi.R.id.signin);
 
-        tvPassword.setOnKeyListener(new View.OnKeyListener() {
+        etPassword.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if(keyCode == 66) {
-                    signin.performClick();
+                    if (checkValidation()) {
+                        signin.performClick();
+                    }
+
                 }
                 return false;
             }
         });
+
+
 
         pref = getApplicationContext().getSharedPreferences("login", 0);
 
@@ -54,12 +82,61 @@ public class Login extends AppCompatActivity {
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] input = new String[2];
-                input[0] = tvUsername.getText().toString();
-                input[1] = tvPassword.getText().toString();
-                new LoginTask(context,view).execute(input);
+                if (checkValidation()) {
+                    String[] input = new String[2];
+                    input[0] = etUsername.getText().toString();
+                    input[1] = etPassword.getText().toString();
+                    new LoginTask(context,view).execute(input);
+                }
+
             }
         });
+    }
+
+    private boolean checkValidation()
+    {
+        boolean ret = true;
+
+        if (!val.hasText(etUsername)) ret = false;
+        if (!val.hasText(etPassword)) ret = false;
+
+        return ret;
+    }
+
+    class Validation {
+
+        //    // Error Messages
+        private static final String REQUIRED_MSG = "required";
+//
+//
+//    // return true if the input field is valid, based on the parameter passed
+//    public static boolean isValid(EditText editText, String regex, String errMsg, boolean required) {
+//
+//        String text = editText.getText().toString().trim();
+//        // clearing the error, if it was previously set by some other values
+//        editText.setError(null);
+//
+//        // text required and editText is blank, so return false
+//        if (required && !hasText(editText) ) return false;
+//
+//        return true;
+//    }
+
+        // check the input field has any text or not
+        // return true if it contains text otherwise false
+        public boolean hasText(EditText editText) {
+
+            String text = editText.getText().toString().trim();
+            editText.setError(null);
+
+            // length 0 means there is no text
+            if (text.length() == 0) {
+                editText.setError(REQUIRED_MSG);
+                return false;
+            }
+
+            return true;
+        }
     }
 
     class LoginTask extends AsyncTask<String, Void, JSONObject> {
@@ -113,17 +190,19 @@ public class Login extends AppCompatActivity {
                         Snackbar.make(view, "Gagal Login. Username/Password salah.", Snackbar.LENGTH_LONG)
                                 .setAction("Action", null).setDuration(Snackbar.LENGTH_LONG).show();
                     }else{
+                        int userId = hasil.getInt("userId");
                         String username = hasil.getString("username");
                         String fullname = hasil.getString("fullname");
-                        Snackbar.make(view, "Selamat datang "+fullname, Snackbar.LENGTH_LONG)
-                                .setAction("Action", null).setDuration(Snackbar.LENGTH_LONG).show();
+                        String hakAkses = hasil.getString("hakAkses");
+//                        Snackbar.make(view, "Selamat datang "+fullname, Snackbar.LENGTH_LONG)
+//                                .setAction("Action", null).setDuration(Snackbar.LENGTH_LONG).show();
 
                         SharedPreferences pref = getApplicationContext().getSharedPreferences("login", 0); // 0 - for private mode
                         Editor editor = pref.edit();
-                        editor.putInt("userId",hasil.getInt("userId"));
-                        editor.putString("hakAkses",hasil.getString("hakAkses"));
-                        editor.putString("username",hasil.getString("username"));
-                        editor.putString("fullname",hasil.getString("fullname"));
+                        editor.putInt("userId",userId);
+                        editor.putString("hakAkses",hakAkses);
+                        editor.putString("username",username);
+                        editor.putString("fullname",fullname);
                         editor.apply();
                         editor.commit();
 
@@ -152,3 +231,4 @@ public class Login extends AppCompatActivity {
     }
 
 }
+
